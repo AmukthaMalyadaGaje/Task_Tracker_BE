54a4eba2879eb6e2b91442d4e9461a37bb183b1b
AmukthaMalyadaGaje <amukthagaje@gmail.com>
2025-05-16 20:05:57 +0530

    fixes!!

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:5173',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
