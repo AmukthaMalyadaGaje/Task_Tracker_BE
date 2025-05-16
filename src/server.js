765a9544e130b30b3e771670fd5eeef3bc61bd17
AmukthaMalyadaGaje <amukthagaje@gmail.com>
2025-05-16 14:03:54 +0530

    Updated files!!

--- src/server.js
+++ src/server.js
@@ -13,6 +13,7 @@ const app = express();
 app.use(cors({
     origin: ['http://localhost:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
