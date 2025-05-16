c51d2cf2efbb172c6e81b474e9badae6440dda17
AmukthaMalyadaGaje <amukthagaje@gmail.com>
2025-05-16 20:00:51 +0530

    fixes!!

--- src/server.js
+++ src/server.js
@@ -16,19 +16,11 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    origin(origin, cb) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            cb(null, true);
-        } else {
-            cb(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: '*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
-
-app.options('*', cors());
 
 app.use(express.json());
 
